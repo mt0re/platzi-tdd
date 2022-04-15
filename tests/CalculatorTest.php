2a81a33f75357717fd286fce24a5df6dbac916d2
mt0re <matiastorezani@gmail.com>
2022-04-14 21:44:58 -0300

    se documentó dentro de Calculator el detalle de las operaciones

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -6,9 +6,32 @@ class CalculatorTest extends TestCase{
     public function testAddWillReturnTheSumOfItsParameters(){
         $sut = new Calculator();
 
+        /* 
+        * Retorna la suma de dos números enteros
+        * @param int $a
+        * @param int $b
+        */
         $this->assertEquals(8, $sut->add(5, 3));
+
+        /* 
+        * Retorna la resta de dos números enteros
+        * @param int $a
+        * @param int $b
+        */
         $this->assertEquals(10, $sut->sub(15, 5));
+
+        /* 
+        * Retorna la división de dos números enteros
+        * @param int $a
+        * @param int $b
+        */
         $this->assertEquals(25, $sut->mult(5, 5));
+
+        /* 
+        * Retorna la multiplicación de dos números enteros
+        * @param int $a
+        * @param int $b
+        */
         $this->assertEquals(10, $sut->div(100, 10));
     }
 }
