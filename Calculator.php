9c957d88984ea3529ad51a56c5a60625f09747f3
mt0re <matiastorezani@gmail.com>
2022-04-14 21:46:35 -0300

    se documentó dentro de Calculator el detalle de las operaciones V2

--- Calculator.php
+++ Calculator.php
@@ -1,19 +1,41 @@
 <?php
 
-class Calculator{
-    public function add($a, $b){
+class Calculator
+{
+    /* 
+        * Retorna la suma de dos números enteros
+        * @param int $a
+        * @param int $b
+        */
+    public function add($a, $b)
+    {
         return $a + $b;
     }
-
-    public function sub($a, $b){
+    /* 
+        * Retorna la resta de dos números enteros
+        * @param int $a
+        * @param int $b
+        */
+    public function sub($a, $b)
+    {
         return $a - $b;
     }
-
-    public function mult($a, $b){
+    /* 
+        * Retorna la multiplicación de dos números enteros
+        * @param int $a
+        * @param int $b
+        */
+    public function mult($a, $b)
+    {
         return $a * $b;
     }
-
-    public function div($a, $b){
+    /* 
+        * Retorna la división de dos números enteros
+        * @param int $a
+        * @param int $b
+        */
+    public function div($a, $b)
+    {
         return $a / $b;
     }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -2,36 +2,16 @@
 
 use PHPUnit\Framework\TestCase;
 
-class CalculatorTest extends TestCase{
-    public function testAddWillReturnTheSumOfItsParameters(){
+class CalculatorTest extends TestCase
+{
+    public function testAddWillReturnTheSumOfItsParameters()
+    {
         $sut = new Calculator();
 
-        /* 
-        * Retorna la suma de dos números enteros
-        * @param int $a
-        * @param int $b
-        */
+
         $this->assertEquals(8, $sut->add(5, 3));
-
-        /* 
-        * Retorna la resta de dos números enteros
-        * @param int $a
-        * @param int $b
-        */
         $this->assertEquals(10, $sut->sub(15, 5));
-
-        /* 
-        * Retorna la división de dos números enteros
-        * @param int $a
-        * @param int $b
-        */
         $this->assertEquals(25, $sut->mult(5, 5));
-
-        /* 
-        * Retorna la multiplicación de dos números enteros
-        * @param int $a
-        * @param int $b
-        */
         $this->assertEquals(10, $sut->div(100, 10));
     }
 }
