9e242e66dae0d1d56a8b16ad94eb71e8d0511327
mt0re <matiastorezani@gmail.com>
2022-04-14 21:34:33 -0300

    se agregaron operaciones a clase Calculator

--- Calculator.php
+++ Calculator.php
@@ -4,4 +4,16 @@ class Calculator{
     public function add($a, $b){
         return $a + $b;
     }
+
+    public function sub($a, $b){
+        return $a - $b;
+    }
+
+    public function mult($a, $b){
+        return $a * $b;
+    }
+
+    public function div($a, $b){
+        return $a / $b;
+    }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -7,6 +7,8 @@ class CalculatorTest extends TestCase{
         $sut = new Calculator();
 
         $this->assertEquals(8, $sut->add(5, 3));
-        $this->assertEquals(10, $sut->add(5, 5));
+        $this->assertEquals(10, $sut->sub(15, 5));
+        $this->assertEquals(25, $sut->mult(5, 5));
+        $this->assertEquals(10, $sut->div(100, 10));
     }
 }
